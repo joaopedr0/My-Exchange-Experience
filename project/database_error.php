1cd39b7315d4d3bd5271cbb52f5f89e49c16f991
joaopedr0 <36745659+joaopedr0@users.noreply.github.com>
2018-11-30 12:38:32 -0800

    Add files via upload

---
+++ project/database_error.php
@@ -0,0 +1,8 @@
+<?php include ('header.php') ?>
+<!DOCTYPE html>
+    <main>
+        <h1>Database Error</h1>
+        <p>There was an error connecting to the database.</p>
+        <p>Error message: <?php echo $error_message; ?></p>
+    </main>
+<?php include ('footer.php') ?>
